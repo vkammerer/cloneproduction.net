c4044481632259bcf39d7a9f300cb0c7ef0d8519
Vincent Kammerer <vkammerer@gmail.com>
2016-04-04 13:03:04 +0800

    final modifs

--- wp-content/themes/marble-child/functions.php
+++ wp-content/themes/marble-child/functions.php
@@ -1,13 +1,15 @@
 <?php
 
-function clone_init() {
-    wp_enqueue_style('clone-first', get_template_directory_uri() . '-child/clone/first.css');
-    wp_enqueue_script('clone-first', get_template_directory_uri() . '-child/clone/first.js');
-}
-function clone_wp_head() {
-    wp_enqueue_style('clone-last', get_template_directory_uri() . '-child/clone/last.css');
-    wp_enqueue_script('clone-last', get_template_directory_uri() . '-child/clone/last.js');
-}
+if ( ! is_admin() ) {
+    function clone_init() {
+        wp_enqueue_style('clone-first', get_template_directory_uri() . '-child/clone/first.css');
+        wp_enqueue_script('clone-first', get_template_directory_uri() . '-child/clone/first.js');
+    }
+    function clone_wp_head() {
+        wp_enqueue_style('clone-last', get_template_directory_uri() . '-child/clone/last.css');
+        wp_enqueue_script('clone-last', get_template_directory_uri() . '-child/clone/last.js');
+    }
 
-add_action( 'init', 'clone_init' );
-add_action( 'wp_head', 'clone_wp_head' );
+    add_action( 'init', 'clone_init' );
+    add_action( 'wp_head', 'clone_wp_head' );
+}
